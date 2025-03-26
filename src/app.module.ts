057305ac81ee75f77198dee27067b3e238322e07
Josimargonga <162895958+Josimargonga@users.noreply.github.com>
2025-03-26 11:44:16 +0100

    priemro commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+
+import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [],
+  providers: [PrismaService],
+})
+export class AppModule {}
